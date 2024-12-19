9bfde48d5ba54152b6f698b4655d89aca83ba34e
Jofila <jofilajohn@gmail.com>
2024-12-19 12:12:42 +0400

    Revert "Pull Request 1"

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -3,7 +3,5 @@ package org.example;
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass");
-        //jofila added this
-        //jofila added this in feature1
     }
 }
