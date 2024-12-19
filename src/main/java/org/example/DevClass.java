22569a97200e2c849e9511dc3d1445687c9f9e65
JofilaJohnShijan <jofilajohn@gmail.com>
2024-12-19 11:44:08 +0400

    CommitFirstChanges

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -3,5 +3,7 @@ package org.example;
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass");
+        //jofila added this
+        //jofila added this in feature1
     }
 }
